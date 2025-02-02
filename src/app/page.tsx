6282a8591324de0d9f857e47e24f7d1f439746d6
Ankit-2145 <ankitsharma2615@gmail.com>
2025-02-02 23:27:48 +0530

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Hero from "@/components/Hero";
 import Navigation from "@/components/Navigation";
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <Hero />
     </>
   );
 }
--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -21,7 +21,7 @@ export function ModeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant="darkModeButton" size="darkModeIcon">
           <MdSunny className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <RxMoon className="absolute h-96 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <RxMoon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
