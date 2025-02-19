17b73b726818a5cf8d6e6bf62efb1c6b6d2825bb
Ankit-2145 <ankitsharma2615@gmail.com>
2025-02-19 23:20:21 +0530

    wip: about content added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,77 +6,44 @@ import { motion } from "motion/react";
 
 const About: React.FC = () => {
   return (
-    <section id="about" className="max-w-4xl min-h-screen mx-4 md:mx-auto">
-      <div className="flex justify-center gap-4">
+    <section
+      id="about"
+      className="min-h-screen bg-[#F5F5F5] dark:bg-[#121212] font-OpenSans border-t-2 pt-24 lg:pt-32"
+    >
+      <div className="flex flex-col md:flex-row justify-evenly items-start gap-4 mx-4 lg:mx-0">
         <div>
-          <h2 className="text-3xl sm:text-4xl font-medium font-OpenSans">
-            About Me{" "}
+          <h2 className="text-lg font-normal leading-relaxed">
+            📂... /About me ...
           </h2>
         </div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0, y: -10 }}
-          whileInView={{ opacity: 1, scale: 1.2, y: 0 }}
-          transition={{
-            delay: 1,
-            duration: 0.8,
-            scale: {
-              type: "spring",
-              bounce: 0.5,
-              ease: "easeOut",
-            },
-          }}
-        >
-          <Image
-            src="/memoji_about.svg"
-            alt="About me memoji"
-            width={36}
-            height={36}
-            className="w-[30px] h-[30px] md:w-9 md:h-9"
-          />
-        </motion.div>
-      </div>
-      <div className="my-8">
-        <p className="text-base text-muted-foreground text-justify font-OpenSans font-medium leading-loose">
-          Hi, I&apos;m{" "}
-          <strong className="text-lime-700 dark:text-lime-400">
-            Ankit Sharma
-          </strong>
-          , a{" "}
-          <strong className="text-highlightTwo dark:text-darkHighlightTwo">
-            Frontend Developer
-          </strong>{" "}
-          and{" "}
-          <strong className="text-indigo-700 dark:text-indigo-400">
-            UI/UX Enthusiast
-          </strong>{" "}
-          with over a year of experience crafting engaging and high-performance
-          web applications. I specialize in building{" "}
-          <strong className="text-highlightOne dark:text-darkHighlightOne">
-            sleek,
-          </strong>{" "}
-          <strong className="text-amber-700 dark:text-amber-300">
-            scalable,
-          </strong>{" "}
-          and{" "}
-          <strong className="text-emerald-700 dark:text-emerald-400">
-            user-friendly interfaces
-          </strong>{" "}
-          using modern technologies like React, Next.js, Tailwind CSS, and
-          Node.js. I have worked on multiple freelance projects, including{" "}
-          <strong className="text-violet-700 dark:text-violet-400">
-            full-stack applications,
-          </strong>{" "}
-          <strong className="text-rose-700 dark:text-rose-400">
-            dynamic websites,
-          </strong>{" "}
-          <strong className="text-orange-700 dark:text-orange-400">LMS,</strong>{" "}
-          <strong className="text-sky-700 dark:text-sky-400">CMS,</strong> and{" "}
-          <strong className="text-pink-700 dark:text-pink-400">
-            research-based portals.
-          </strong>{" "}
-          My goal is to create seamless digital experiences that are both
-          visually appealing and functionally robust.
-        </p>
+        <div className="max-w-xl">
+          <p className="text-base text-muted-foreground text-left font-normal leading-relaxed mt-4 md:mt-0">
+            Hello, I&apos;m{" "}
+            <span className="font-medium italic text-lime-700 dark:text-lime-400">
+              Ankit Sharma
+            </span>
+            , a{" "}
+            <span className="font-medium italic text-highlightTwo dark:text-darkHighlightTwo">
+              Frontend Developer
+            </span>{" "}
+            and{" "}
+            <span className="font-medium italic text-indigo-700 dark:text-indigo-400">
+              UI/UX Enthusiast
+            </span>{" "}
+            with over a year of experience. I specialize in crafting{" "}
+            <span className="font-medium italic text-highlightOne dark:text-darkHighlightOne">
+              sleek,
+            </span>{" "}
+            <span className="font-medium italic text-amber-700 dark:text-amber-300">
+              scalable,
+            </span>{" "}
+            and{" "}
+            <span className="font-medium italic text-emerald-700 dark:text-emerald-400">
+              user-friendly interfaces
+            </span>{" "}
+            using React, Next.js, Tailwind CSS, and Node.js.
+          </p>
+        </div>
       </div>
     </section>
   );
